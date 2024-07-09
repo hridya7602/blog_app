2d4f6ba596149e781bb6589126c4530121f2b59c
anabeth-chase <hridyaharshan@gmail.com>
2024-07-09 16:16:39 +0530

    Add all the basic pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import {BrowserRouter , Routes, Route} from 'react-router-dom';
 
+import Home from "./pages/Home"
+import React from 'react'
+import About from "./pages/About"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/signin' element={<SignIn />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/projects' element={<Projects />} />
+
+    </Routes>
+    </BrowserRouter>
   )
 }
 
